2a0193a6afde30e1ff1c30f6ca8ff0208c6c8f16
alexandraverasalinas <alexandraverasalinas@gmail.com>
2024-04-03 23:37:00 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Weather App
-      </header>
-    </div>
+      <h1> Weather App </h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/alexandraverasalinas"
+            target="_blank" rel="noreferrer"
+          >
+            Alexandra Vera
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/alexandraverasalinas/REACT-Weather-App"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
   );
 }
-
-export default App;
